8db66510b42e30fb4c664dbdad2e5cc913ec9fe8
andre2xu <>
2024-06-10 12:26:49 +0100

    Created a PHP file for the index page that has a form for uploading resumes.

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>PHP Resume Parser</title>
+</head>
+<body>
+    <form action="/" method="post" enctype="multipart/form-data">
+        <input type="file" name="resume_upload[]" id="resume_upload" multiple>
+
+        <button type="submit">Upload</button>
+    </form>
+</body>
+</html>
+
+<?php
+    
+?>

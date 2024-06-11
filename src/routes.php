4fe0c861e8e717733ba04b3fd80ded94913ccf7f
andre2xu <>
2024-06-11 12:56:10 +0100

    Created a file for routing. Added a route for the login controller.

---
+++ src/routes.php
@@ -0,0 +1,21 @@
+<?php
+    use Symfony\Component\Routing;
+
+
+
+    // import all controllers
+    $controller_files = glob(__DIR__ . '/controllers/*.php');
+
+    foreach ($controller_files as $file) {
+        require_once $file;
+    }
+
+    // create routes
+    $routes = new Routing\RouteCollection();
+
+    $routes->add('login', new Routing\Route('/login', array(
+        '_controller' => 'Controllers\LoginController::login'
+    )));
+
+    return $routes;
+?>

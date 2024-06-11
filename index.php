d35fa3cf406cda3f811133ab9667e87755877975
andre2xu <>
2024-06-11 12:58:12 +0100

    Implemented the front controller logic in the index file.

--- index.php
+++ index.php
@@ -16,5 +16,41 @@
 </html>
 
 <?php
-    
+    require_once __DIR__ . '/vendor/autoload.php';
+
+    use Symfony\Component\HttpFoundation\Request;
+    use Symfony\Component\HttpFoundation\Response;
+    use Symfony\Component\HttpKernel;
+    use Symfony\Component\Routing;
+
+
+
+    $routes = include __DIR__ . '/src/routes.php';
+
+    $request = Request::createFromGlobals();
+
+    $context = new Routing\RequestContext();
+    $context->fromRequest($request);
+
+    $matcher = new Routing\Matcher\UrlMatcher($routes, $context);
+
+    $controllerResolver = new HttpKernel\Controller\ControllerResolver();
+    $argumentResolver = new HttpKernel\Controller\ArgumentResolver();
+
+    try {
+        $request->attributes->add($matcher->match($request->getPathInfo()));
+
+        $controller = $controllerResolver->getController($request);
+        $arguments = $argumentResolver->getArguments($request, $controller);
+
+        $response = call_user_func_array($controller, $arguments);
+    } 
+    catch (Routing\Exception\ResourceNotFoundException $exception) {
+        $response = new Response('Not Found', 404);
+    } 
+    catch (Exception $exception) {
+        $response = new Response('Internal Server Error', 500);
+    }
+
+    $response->send();
 ?>

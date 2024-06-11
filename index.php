13b3017b48e5533a1aab6eeb7557266866dd2718
andre2xu <>
2024-06-11 12:59:46 +0100

    Removed the resume upload form from the index file.

--- index.php
+++ index.php
@@ -7,11 +7,6 @@
     <title>PHP Resume Parser</title>
 </head>
 <body>
-    <form action="/" method="post" enctype="multipart/form-data">
-        <input type="file" name="resume_upload[]" id="resume_upload" multiple>
-
-        <button type="submit">Upload</button>
-    </form>
 </body>
 </html>
 

7ef183790bfc194fa0feba7e8eab3d48dd65c633
andre2xu <>
2024-06-13 21:21:31 +0100

    Implemented the sign-up logic.

--- src/controllers/LoginController.php
+++ src/controllers/LoginController.php
@@ -2,6 +2,7 @@
     namespace Controllers;
 
     use Symfony\Component\HttpFoundation;
+    use Services;
 
 
 
@@ -18,6 +19,30 @@
                 $user_password = $request->request->get('password');
 
                 // check if an account with the given username already exists
+                $db = Services\db();
+
+                $sql_statement = $db->prepare('SELECT * FROM users WHERE email = ? LIMIT 1');
+                $sql_statement->execute([$user_email]);
+
+                $email_exists = $sql_statement->fetch();
+
+                if ($email_exists == false) {
+                    $user_password = hash('sha256', $user_password);
+
+                    // create an account
+                    $db->beginTransaction();
+
+                    $sql_statement = $db->prepare('INSERT INTO users (email, password) VALUES (?, ?)');
+
+                    $sql_statement->execute([$user_email, $user_password]);
+
+                    $db->commit();
+                }
+                else {
+                    $response = $this->generateTemplateResponse('signup.html', array(
+                        'message' => 'That email already exists'
+                    ));
+                }
             }
 
             return $response;

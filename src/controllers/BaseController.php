977480c31eec8bbfccbe872081fa99f90874ecbe
andre2xu <>
2024-06-14 14:18:03 +0100

    Added another method to the base controller that generates a redirect response.

--- src/controllers/BaseController.php
+++ src/controllers/BaseController.php
@@ -2,7 +2,10 @@
     namespace Controllers;
 
     use Symfony\Component\HttpFoundation;
+    use Symfony\Component\Routing;
     use Twig;
+
+    $routes = include __DIR__ . '/../routes.php';
 
 
 
@@ -18,6 +21,16 @@
 
         protected function generateTemplateResponse(string $path, array $context = []): HttpFoundation\Response {
             return new HttpFoundation\Response($this->twig->render($path, $context));
+        }
+
+        protected function generateRedirectResponse(HttpFoundation\Request $request, string $routeName, array $context = [], int $statusCode = 301): HttpFoundation\RedirectResponse {
+            $request_context = new Routing\RequestContext();
+            $request_context->fromRequest($request);
+
+            global $routes;
+            $url_generator = new Routing\Generator\UrlGenerator($routes, $request_context);
+
+            return new HttpFoundation\RedirectResponse($url_generator->generate($routeName, $context), $statusCode);
         }
     }
 ?>

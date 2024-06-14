950a21ed7d40d56c2b7725f278fd7f17e7130b27
andre2xu <>
2024-06-14 18:45:43 +0100

    Deleted the HTML in the index file.

--- index.php
+++ index.php
@@ -1,15 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-    <title>PHP Resume Parser</title>
-</head>
-<body>
-</body>
-</html>
-
 <?php
     require_once __DIR__ . '/vendor/autoload.php';
     require_once __DIR__ . '/src/services.php';

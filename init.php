90eeec238683ea9bffda9325c0f773f0dfa301a0
andre2xu <>
2024-06-15 18:25:20 +0100

    Added an id to the 'users' table.

--- init.php
+++ init.php
@@ -6,8 +6,10 @@
         $db->exec('
             DROP TABLE IF EXISTS users;
             CREATE TABLE users (
+                id int NOT NULL AUTO_INCREMENT,
                 email varchar(255) NOT NULL,
-                password varchar(64) NOT NULL
+                password varchar(64) NOT NULL,
+                PRIMARY KEY (id)
             );
         ');
     }

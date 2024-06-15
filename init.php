500eb780113af6b4e3a71b8b9d48a46d12921218
andre2xu <>
2024-06-15 19:36:43 +0100

    The SQL statement that initializes the database now adds a test user.

--- init.php
+++ init.php
@@ -19,6 +19,12 @@
                 filename varchar(44) NOT NULL,
                 FOREIGN KEY (user_id) REFERENCES users(id)
             );
+
+
+
+            -- for development
+            INSERT INTO users (email, password) VALUES
+            ("a@a.com", "6b86b273ff34fce19d6b804eff5a3f5747ada4eaa22f1d49c01e52ddb7875b4b");
         ');
     }
     catch (\PDOException $error) {
